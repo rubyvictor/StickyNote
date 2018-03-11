c602f7d9f17a0984c036fbc80eb7db5146c4dcc4
Victor Lee <myleevictor@gmail.com>
2018-03-11 13:02:19 +0800

    refactored function to constructor to initialize the app

--- scripts/StickyNotesApp.js
+++ scripts/StickyNotesApp.js
@@ -1,10 +1,32 @@
+"use Strict";
+
 class StickyNotesApp {
-constructor(){
-    
-}
+  constructor() {
+    this.notesContainer = document.getElementById("notes-container");
+    this.noteMessageInput = document.getElementById("message");
+    this.addNoteButton = document.getElementById("save");
+    this.notesSectionTitle = document.getElementById("notes-section-title");
 
+    // Saves notes on button click.
+    this.addNoteButton.addEventListener("click", this.saveNote.bind(this));
 
+    // Toggle for the button.
+    this.noteMessageInput.addEventListener(
+      "keyup",
+      this.toggleButton.bind(this)
+    );
 
+    // Loads all the notes.
+    for (var key in localStorage) {
+      this.displayNote(key, localStorage[key]);
+    }
 
-
+    // Listen for updates to notes from other windows.
+    window.addEventListener(
+      "storage",
+      function(e) {
+        this.displayNote(e.key, e.newValue);
+      }.bind(this)
+    );
+  }
 }
